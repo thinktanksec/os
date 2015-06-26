4be89b1de72ef4885ab1e4e2358e3b4a5a541691
Zack Deveau <zack.netsec@gmail.com>
2015-06-26 17:35:54 -0300

    Update process.c

--- process.c
+++ process.c
@@ -1,178 +1,514 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+/*-------------------------
+	Simple reset
+--------------------------*/
+/*@import url("http://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.5.4/bootstrap-select.min.css")
+@import url("http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css")
+@import url("http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css")
+*/
+
+*{
+	margin:0;
+	padding:0;
+}
 
 
-//====================
-//GLOBALS
-typedef unsigned int PID;  
-PID pidIndex = 0;
+/*-------------------------
+	General Styles
+--------------------------*/
 
 
-//=================
-//Structs
-typedef struct proc_t
-{
-	//Used to ID the process
-	PID PID; //<---- :o 
+html{
+	overflow:hidden;
+}
 
-	//Thread handle
-  	pthread_t *thread;
+a, a:visited {
+	outline:none;
+	color:#389dc1;
+}
 
-    //Lock
+a:hover{
+	text-decoration:none;
+}
 
-    //Argument
-    int arg;
-	
-	//For semaphore queue
-	struct sem_t* next;
-
-	//Traverse the doubly linked list.
-	struct Node* next;
-	struct Node* prev;
-}process;
+section, footer, header, aside{
+	display: block;
+}
 
 
+/*-------------------------
+	The main page elements
+--------------------------*/
+
+.modal-header {
+    padding:9px 15px;
+    border-bottom:2px solid #eee;
+    background-color: #ffcd8a;
+    -webkit-border-top-left-radius: 5px;
+    -webkit-border-top-right-radius: 5px;
+    -moz-border-radius-topleft: 5px;
+    -moz-border-radius-topright: 5px;
+     border-top-left-radius: 5px;
+     border-top-right-radius: 5px;
+ }
+
+ .modal-body {
+    padding:9px 10px;
+    height: 40px;
+    background-color: #ffbc63;
+    -webkit-border-bottom-left-radius: 5px;
+    -webkit-border-bottom-right-radius: 5px;
+    -moz-border-radius-bottomleft: 5px;
+    -moz-border-radius-bottomright: 5px;
+     border-bottom-left-radius: 5px;
+     border-bottom-right-radius: 5px;
+ }
 
 
-//Head of our main process list.
-process* sporadicHeadPCB = NULL;
 
+#drop {
+    border: 3px dashed #ccc;
+    width: 80%;
+	height: 80%; 
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    border-radius: 4px;
+    display:box;
+    cursor:pointer;
+    margin-left: auto ;
+  	margin-right: auto ;
+  	position: relative;
+  	top:55px;
+}
 
+#status {
+    width:450px;
+    margin: 0 auto;
+}
 
+#list {
+    width:450px;
+    margin: 0 auto;
+}
 
-//==================
-//Queue Functions
-/*
- * The semaphores work as a giant queue. The masterSem is the master head of the semaphore queue.
- * Each semaphore that is created gets appended to the cue that masterSem is the head of.
- */
-int enqueueProcess(process **head, process *newProcess)
-{
-    process *tmp;
+.msg-drop{
+	text-align:center;
+    padding:10px;
+}
 
-    if (newProcess == NULL)
-        return -1;
+.thumb {
+    width:33px;
+    height:33px;
+    float:left;
+    margin:3.5px;
+    border: 1px solid #ccc;
+}
 
-    //Case where it is the first one in the list.
-    if (*head == NULL)
-    {
-        *head = newProcess;
-        newProcess->next = NULL;
-    }
-    //Else the list is not empty, append this sem to the tail.
-    else
-    {
-        tmp = *head;
-        while (tmp->next != NULL)
-            tmp = tmp->next;
+.fileCont{
+    display:block;
+    width:425px;
+    height:40px;
+    margin:2.5px;
+    float:left;
+}
+.fileSize{
+   text-align:right;
+    margin:2.5px;
+    font-size:12px;
+    padding-right: 10px;
+}
+.fileName{
+    float:left;
+    padding:2.5px;
+    font-weight:700;
+    text-transform: capitalize;    
+}
+.remove{
+    width:20px;
+    height:40px;
+    padding-top: 13px;
+    font-size: 18px;
+    float:left;
+}
 
-        tmp->next = newProcess;
-        newProcess->next = NULL;
-    }
-    return 1;
+#fileBox{
+    display:none;
 }
 
 
 
 
-process* dequeueProcess(process **head)
-{
-    process *tmp = NULL;
 
-    if (*head != NULL)
-    {
-        tmp = *head;
-        *head = (*head)->next;
-    }
-    return tmp;
+
+
+
+
+
+.progress-bar-info {
+  background-color: #ff9063;
+}
+
+
+body{
+	font:15px/1.3 'Raleway', sans-serif;
+	color: #fff;
+	width:100%;
+	height:100%;
+	position:absolute;
+	overflow:hidden;
+}
+
+
+#stage{
+	width:100%;
+	height:100%;
+	position:absolute;
+	top:0;
+	left:0;
+
+	transition:top 0.6s;
+}
+
+#stage > div{  /* The step divs */
+	height:100%;
+	position:relative;
+}
+
+
+#stage h1{
+	font-weight:normal;
+	font-size:48px;
+	text-align:center;
+	color:#fff;
+	margin-bottom:60px;
+	text-shadow: 3px 4px 0 rgba(0,0,0,0.08);
+}
+
+#stage h2{
+	font-weight: normal;
+	font-size: 14px;
+	font-family: Arial, Helvetica, sans-serif;
+	margin: -40px 0 45px;
+	font-style: italic;
+	text-shadow: 3px 4px 0 rgba(0,0,0,0.08);
+}
+
+
+#stage h4{
+	font-weight:normal;
+	font-size:58px;
+	text-align:center;
+	color:#fff;
+	margin-bottom:20px;
+	text-shadow: 3px 4px 0 rgba(0,0,0,0.08);
+}
+
+#stage h5{
+	font-weight: normal;
+	font-size: 14px;
+	font-family: Arial, Helvetica, sans-serif;
+	margin: 30px 0 15px;
+	font-style: italic;
+	text-shadow: 3px 4px 0 rgba(0,0,0,0.08);
+}
+
+#stage p2{
+	font-size:11px;
+	color:#ffe;
+	text-shadow: 3px 4px 0 rgba(0,0,0,0.08);
+}
+
+
+
+.tos{
+    width: 100px;
+    height: 100px;
+    position: relative;
+    left: 50%;
+    margin-left: -128px;
+    margin-top: 25px;
+    float: left;
+    clear: none;
+}
+
+
+.how{
+    width: 100px;
+    height: 100px;
+    position: relative;
+    right: 50%;
+    margin-right: -60px;
+    margin-top: 25px;
+    float: right;
+    clear: none;
+}
+
+
+.content{
+	position:absolute;
+	text-align:center;
+	left:0;
+	top:40%;
+	width:100%;
+}
+
+.content2{
+	position:absolute;
+ 	margin-left: 20px;
+	left:0;
+	top:15%;
+	width:500px;
+}
+
+.content3{
+	position:absolute;
+ 	margin-left: 20px;
+	left:0;
+	top:55%;
+	width:500px;
+}
+
+.proj1{
+	position:absolute;
+	text-align:center;
+	top:5%;
+	margin-left: 20px;
+}
+
+.links{
+
+
+	margin-top: 35px;
+	margin-left: 30px;
+	display: block;
+} 
+
+
+.info{
+	font-size: 13px;
+}
+
+
+.bitcoin{
+    width: 100px;
+    height: 100px;
+    position: relative;
+    left: 50%;
+    margin-left: -133px;
+    float: left;
+    clear: none;
+}
+
+.paypal{
+    width: 100px;
+    height: 100px;
+    position: relative;
+    right: 50%;
+    margin-right: -138px;
+    float: right;
+    clear: none;
+}
+
+
+button, input, optgroup, select, textarea {
+  margin: 10;
+  font: inherit;
+  color: #c15645;
+}
+
+.content input[type=file]{
+	display:none;
+}
+
+a.back{
+	width: 32px;
+	height: 32px;
+	background: url('../img/icons.svg') no-repeat -192px 0;
+	position: absolute;
+	cursor: pointer;
+	top: 17px;
+	right: 17px;
+	z-index: 10;
+	opacity: 0.8;
+	margin-left: -16px;
+	display:none;
+}
+
+a.back:hover{
+	opacity:1;
+}
+
+
+#stage p3{
+	font-weight: normal;
+	font-size: 12px;
+	font-family: Arial, Helvetica, sans-serif;
+	font-style: italic;
+	text-shadow: 3px 4px 0 rgba(0,0,0,0.08);
+}
+
+/*-------------------------
+	Conditional classes
+--------------------------*/
+
+
+[class*="if-"]{
+	display:none;
+}
+
+body.encrypt .if-encrypt{
+	display:block;
+}
+
+body.decrypt .if-decrypt{
+	display:block;
+}
+
+
+/*-------------------------
+	Button styles
+--------------------------*/
+
+
+.button{
+	width:240px;
+	height:70px;
+	text-align:center;
+	text-decoration: none !important;
+	color:#fff !important;
+	text-transform: uppercase;
+	font-weight: bold;
+	border-radius:1px;
+	display:block;
+	line-height:70px;
+	box-shadow:3px 3px 0 rgba(0,0,0,0.08);
+	cursor: pointer;
+	font-size:18px;
+	margin:10px auto;
+	opacity:0.9;
+}
+
+.button:hover{
+	opacity:.7;
+}
+
+.button::before{
+	content:'';
+	background: url(../img/icons.svg) no-repeat;
+	display: inline-block;
+	width: 32px;
+	height: 32px;
+	vertical-align: middle;
+	padding-right: 13px;
+}
+
+.button.green{
+	background-color:#63535b;
+}
+
+.button.magenta{
+	background-color:#fabf7f;
+}
+
+.button.blue{
+	background-color:#ddae7e;
+}
+
+.button.red{
+	background-color: #FF725C;
+}
+
+/* Styles for specific buttons */
+
+.button.browse{
+	width:180px;
+}
+
+.button.process{
+	width:190px;
+}
+
+.button.download{
+	width:216px;
+}
+
+.button.encrypt::before{
+	background-position: 0 0;
+}
+
+.button.decrypt::before{
+	background-position: -96px 0;
+}
+
+.button.browse::before{
+	background-position: -240px 0;
+} 
+
+.button.process::before{
+	background-position: -142px 0;
+}
+
+.button.download:before{
+	background-position:-48px 0;
+}
+
+
+/*-------------------------
+	The steps
+--------------------------*/
+
+
+#step1{
+	background-color:#acc18a;
+}
+
+body.encrypt #step2{
+	background-color: #63535b;
+}
+
+body.decrypt #step2{
+	background-color: #fcd0a1;
+}
+
+#step3{
+	background-color: #63535b;
+}
+
+#step4{
+	background-color:#79BD9A;
+}
+
+#step4 input[type=password]{
+	background-color: #fff;
+	border: none;
+	padding: 8px 18px;
+	line-height: 1;
+	font: inherit;
+	display: inline-block;
+	outline: none;
+	width: 300px;
+	margin-bottom: 18px;
+	border-radius: 2px;
+	box-shadow: 3px 3px 0 rgba(0,0,0,0.05);
+	font-size: 36px;
+	color: #555;
+}
+
+#step5{
+	background-color:#7E7F9A;
+}
+
+#step6{
+	background-color:#c11b17;
 }
 
 
 
 
-//Given a process queue, it will find the matching process
-//for a given PID
-process* findPID(process *head, int PID)
-{
-    semaphore *tmp = head;
-    while (tmp != NULL)
-        if (tmp->PID == PID)
-            return tmp;
-        else
-            tmp = tmp->next;
-    return tmp;
-}
+#step1 .content{	margin-top: -140px;}
+#step2 .content{	margin-top:-110px;}
+#step3 .content{	margin-top:-100px;}
+#step4 .content{	margin-top: -157px;}
+#step5 .content{	margin-top: -100px;}
+#step6 .content{	margin-top: -100px;}
 
 
-
-
-
-PID OS_Create(void (*f)(void), int arg, unsigned int level, unsigned int n)
-{
-    pidIndex += 1;
-
-    //Allocate and create 
-    process *proc = (pthread_t *)malloc(sizeof(pthread_t);
-
-    //Set its thread to operate on the given funciton pointer
-    pthread_create(&(proc->thread), NULL, &f, NULL);
-
-	//Set some fields
-	proc->PID = pidIndex;
-
-    //Set param
-    proc->arg = arg;
-
-	//Append to the appropriate queue
-	enqueueProcess(&sporadicHeadPCB, proc);
-
-	return pidIndex;
-}
-
-
-
-void OS_Terminate(void)
-{
-
-}    
-
-
-void OS_Yield(void)
-{
-
-}
-
-
-int  OS_GetParam(void)
-{
-
-}
-
-
-void main()
-{
-    pthread_t   *threads[3];
-
-    OS_InitSem(69, 1);
-
-    sem_init(&sem_name, 0, 1);
-    puts("we have inited");
-
-    int i = 0;
-    for (i = 0; i < 3; i++)
-        threads[i] = (pthread_t *)malloc(sizeof(pthread_t));
-    for (i = 0; i < 3; i++)
-        pthread_create(&(threads[i]), NULL, &test, i);
-    for (i = 0; i < 3; i++)
-        pthread_join(threads[i], NULL);
-
-    return;
-
-}
